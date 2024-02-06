67d2868331dec6db239374c396d5d5645c6bc3b1
Firmansyah Putra Pratama <119718542+Firmansyahp16@users.noreply.github.com>
2024-02-06 20:14:28 +0700

    Delete management directory

--- management/api/configs/dbConfig.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(process.env.URL, process.env.KEY);
-
-module.exports = supabase;
--- management/ui/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template lang="">
-  <div>
-    <h1 class="text-red-500">Index</h1>
-  </div>
-</template>
